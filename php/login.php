2b7d926f5496020d806117b4f4b3feba1aaa0713
Balinth <balinth.cg@gmail.com>
2021-04-24 16:47:45 +0200

    Login error handling

--- php/login.php
+++ php/login.php
@@ -1,10 +1,24 @@
 <?php
     session_start();
+    session_unset();
     include_once "todo_user.php";
 
     if(isset($_POST["logineml"]) && isset($_POST["loginpw"])){
-        $_SESSION["user"] = new User("fos","fos","fos@fos","fos.png");
-        header("Location: todo_list.php");
-    }
 
+        if($_POST["logineml"] == 'admin@admin.admin'){
+            if ($_POST['loginpw'] == 'admin'){
+                $_SESSION["user"] = new User("admin","admin","admin@admin.admin","fos.png");
+                header("Location: todo_list.php");
+                return;
+            }
+            else {
+                $_SESSION['loginpwError'] = true;
+            }
+
+        }
+        else {
+            $_SESSION['loginemlError'] = true;
+        }
+    }
+    header("Location: index.php");
 ?>
